0acb138bc6752bfa6b6e5e7d3ed3f2f2a2ca7a6c
hasibulislam999 <hasib143sl@gmail.com>
2022-11-24 15:40:42 +0600

    review service init

---
+++ services/review.service.js
@@ -0,0 +1,39 @@
+/* internal export */
+const Review = require("../schemas/review.schema");
+
+/* insert new review */
+exports.insertNewReview = async (data) => {
+  const result = await Review.create(data);
+  return result;
+};
+
+/* display all reviews */
+exports.displayAllReviews = async ({ page }) => {
+  const contentLimit = 3;
+  const result = await Review.find()
+    .skip((Number(page) - 1) * contentLimit)
+    .limit(page && contentLimit);
+  return result;
+};
+
+/* display specific review */
+exports.displaySpecificReview = async ({ id }) => {
+  const result = await Review.findById(id);
+  return result;
+};
+
+/* update specific review */
+exports.updateSpecificReview = async (id, data) => {
+  const result = await Review.updateOne(
+    { _id: id },
+    { $set: data },
+    { upsert: true, runValidators: true }
+  );
+  return result;
+};
+
+/* remove specific review */
+exports.removeSpecificReview = async ({ id }) => {
+  const result = await Review.findByIdAndDelete(id);
+  return result;
+};
