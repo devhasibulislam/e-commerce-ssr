2b1a7c4b7d8d4f4db30b4b58b804237efc64bd22
hasibulislam999 <hasib143sl@gmail.com>
2022-11-24 10:12:49 +0600

    blog controller init

---
+++ controllers/blog.controller.js
@@ -0,0 +1,126 @@
+/**
+ * Title: Blog controller
+ * Description: Control request, response and other middlewares
+ * Author: Hasibul Islam
+ * Date: 24/11/2022
+ */
+
+/* internal import */
+const blogService = require("../services/blog.service");
+
+/* upload a blog thumbnail */
+exports.blogThumbnailUpload = (req, res, next) => {
+  try {
+    const result = req.file;
+
+    res.status(201).json({
+      acknowledgement: true,
+      message: "Created",
+      description: "Blog thumbnail uploaded successfully",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/* update a blog thumbnail */
+exports.blogThumbnailUpdate = async (req, res, next) => {
+  try {
+    await blogService.blogThumbnailUpdate(req.query.filename);
+    const result = req.file;
+
+    res.status(202).json({
+      acknowledgement: true,
+      message: "Accepted",
+      description: "Blog thumbnail updated successfully",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/* insert a new blog */
+exports.insertNewBlog = async (req, res, next) => {
+  try {
+    const result = await blogService.insertNewBlog(req.body);
+
+    res.status(201).json({
+      acknowledgement: true,
+      message: "Created",
+      description: "Blog insertion successful",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/* display all blogs */
+exports.displayAllBlogs = async (req, res, next) => {
+  try {
+    const result = await blogService.displayAllBlogs(req.query);
+
+    res.status(200).json({
+      acknowledgement: true,
+      message: "OK",
+      description: "Successfully fetching all blogs",
+      count: result.length,
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/* display specific blog */
+exports.displaySpecificBlogs = async (req, res, next) => {
+  try {
+    const result = await blogService.displaySpecificBlogs(req.params);
+
+    res.status(200).json({
+      acknowledgement: true,
+      message: "OK",
+      description: "Successfully fetching specific blog",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/* update specific blog */
+exports.updateSpecificBlog = async (req, res, next) => {
+  try {
+    const result = await blogService.updateSpecificBlog(
+      req.params.id,
+      req.body
+    );
+
+    res.status(202).json({
+      acknowledgement: true,
+      message: "Accepted",
+      description: "Successfully update specific blog",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/* remove specific blog */
+exports.removeSpecificBlog = async (req, res, next) => {
+  try {
+    const result = await blogService.removeSpecificBlog(req.params);
+
+    res.status(202).json({
+      acknowledgement: true,
+      message: "Accepted",
+      description: "Successfully removed specific blog",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
